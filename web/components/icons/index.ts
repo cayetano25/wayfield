c6070090045476e1f01fb2a9c2a4bf3e1a077704
Tomas Alvarez <tomasalvarez@Tomass-Mac-mini.local>
2026-05-03 14:00:06 -0500

    feat(icons): add barrel export index

---
+++ web/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as ParticipantIcon } from './ParticipantIcon';
+export { default as OrganizerIcon } from './OrganizerIcon';
+export { default as AnyFormatIcon } from './AnyFormatIcon';
+export { default as OfflineReadyIcon } from './OfflineReadyIcon';
+export type { IconProps } from './types';
